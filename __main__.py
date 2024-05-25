98343077cfeed4950d424682b5105f1dd529ffb1
Keith Bishop <bishopofkeith@proton.me>
2024-05-25 05:51:46 -0500

    index template wip

--- __main__.py
+++ __main__.py
@@ -1,21 +1,28 @@
 """ FastAPI + HTMLX """
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 # Routers
 from .routers.pages import router as pages_router
 
 app = FastAPI()
 app.include_router(pages_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def read_root():
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
     """Welcome message"""
-    return {"message": "Welcome to the API!"}
+    return templates.TemplateResponse(request=request, name="index.html")
 
 def main():
     """Run the server using uvicorn"""
-    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info", reload=True)
+    print("hey")
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info", reload=True)
 
 if __name__ == "__main__":
     main()
